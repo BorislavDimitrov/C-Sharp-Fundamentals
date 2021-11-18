8e8606a6437742fdc5380961fe426da56b999cf0
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2021-11-18 13:36:04 +0200

    Add exercises

---
+++ FundExamPrep/AdAstra/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.AdAstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex pattern = new Regex(@"(\||\#)(?<itemName>[A-Za-z\s]+)\1(?<expireDate>[0-9]{2}\/[0-9]{2}\/[0-9]{2})\1(?<calories>[0-9]+)\1");
+            MatchCollection matches = pattern.Matches(input);
+            int totalCalories = 0;
+
+            foreach (Match item in matches)
+            {
+                totalCalories += int.Parse(item.Groups["calories"].Value);
+            }
+
+            int daysToSurvive = totalCalories / 2000;
+
+            Console.WriteLine($"You have food to last you for: {daysToSurvive} days!");
+
+            foreach (Match item in matches)
+            {
+                Console.WriteLine($"Item: {item.Groups["itemName"].Value}" +
+                    $", Best before: {item.Groups["expireDate"].Value}, Nutrition: {item.Groups["calories"].Value}");
+            }
+        }
+    }
+}
