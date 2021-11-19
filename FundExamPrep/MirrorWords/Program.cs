a64c3db90f63fe5db515c87dc6099d64fbf87f31
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2021-11-19 13:37:13 +0200

    Add exercises

---
+++ FundExamPrep/MirrorWords/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MirrorWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex pattern = new Regex(@"(\#|\@)(?<firstWord>[A-Za-z]{3,})\1\1(?<secondWord>[A-Za-z]{3,})\1");
+            MatchCollection pairs = pattern.Matches(input);
+            Dictionary<string, string> validPairs = new Dictionary<string, string>();
+
+            foreach (Match pair in pairs)
+            {
+                string currFirstWord = pair.Groups["firstWord"].Value;
+                string currSecondWord = pair.Groups["secondWord"].Value;
+                string reversedSecondWord = new string(currSecondWord.Reverse().ToArray());
+
+                if (currFirstWord == reversedSecondWord)
+                {
+                    validPairs.Add(currFirstWord , currSecondWord);
+                }
+            }
+
+            if (pairs.Count > 0)
+            {
+                Console.WriteLine($"{pairs.Count} word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+
+            if (validPairs.Count > 0)
+            {
+                Console.WriteLine("The mirror words are:");
+            }
+            else
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            
+            int counter = 0;
+
+            foreach (var pair in validPairs)
+            {
+                counter++;
+
+                if (counter < validPairs.Count)
+                {
+                    Console.Write(pair.Key + " <=> " + pair.Value + ", ");
+                }
+                else
+                {
+                    Console.Write(pair.Key + " <=> " + pair.Value);
+                }
+            }
+        }
+    }
+}
